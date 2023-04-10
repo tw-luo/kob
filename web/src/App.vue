61fdaa98ea55ca8e401a0014ae2891f8a3623b81
tw_luo <2505412775@qq.com>
2023-04-10 14:29:32 +0800

    修改基础设置

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,7 +3,9 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view>
+
+  </router-view>
 </template>
 
 <style>
