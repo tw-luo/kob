aaad3400bb224e3af6e02994b6f71dbca762609c
tw_luo <2505412775@qq.com>
2023-04-10 14:53:28 +0800

    try to connect api

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,42 @@
 <template>
-  <div class="container">
-    Hello World
+  <div>
+    <div>bot name:{{ botName }}</div>
+    <div>bot level: {{ botLevel }}</div>
   </div>
   <router-view>
 
   </router-view>
 </template>
 
+<script>
+import $ from 'jquery';
+import {ref} from 'vue';
+
+let botName = ref('');
+let botLevel = ref('');
+
+export default{
+  name: 'App',
+  setup(){
+    $.ajax({
+      url: 'http://localhost:8088/pk/api/get_bot_info',
+      type: 'GET',
+      dataType: 'json',
+      success: function(data){
+        botName.value = data.name;
+        botLevel.value = data.level;
+      }
+    });
+    return {
+      botName,
+      botLevel
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
 
