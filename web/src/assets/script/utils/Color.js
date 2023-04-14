375025a9314040cd3305ecab2935d30b537afabd
tw_luo <2505412775@qq.com>
2023-04-14 11:25:10 +0800

    添加蛇的颜色

--- web/src/assets/script/utils/Color.js
+++ web/src/assets/script/utils/Color.js
@@ -2,6 +2,10 @@ const Color = {
   evenSquare: "#AAD751",
   oddSquare: "#A2D149",
   wall:"#B37226",
+  snakeDie:"white",
+  snakeEye:"black",
+  snake1:"#FF0000",
+  snake2:"blue",
 };
 
 export default Color;
