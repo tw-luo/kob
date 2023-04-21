5a3c0fa287e4691dbdc462b1e5f02d8bbe9032f7
tw_luo <2505412775@qq.com>
2023-04-21 23:17:40 +0800

    对bot部分的add api进行测试

--- web/src/views/user/bot/UserBotView.vue
+++ web/src/views/user/bot/UserBotView.vue
@@ -9,13 +9,41 @@
 <script>
 
 import ContentBase from '@/components/ContentBase.vue';
-
+import $ from 'jquery';
+import {useStore} from 'vuex';
 
 export default {
     name: 'UserBotView',
     components: {
         ContentBase
     },
+    setup(){
+        const store=useStore();
+        $.ajax({
+            url: "http://127.0.0.1:8088/user/bot/add/",
+                type: "post",
+                data: {
+                    bot_name: "bot name",
+                    description: "bot description",
+                    content: "bot content",
+                },
+                headers:{
+                    Authorization: "Bearer "+store.state.user.token,
+                },
+                success(resp) {
+                    console.log(resp);
+                    if (resp.message === "success") {
+                        alert("添加成功");
+                    } else {
+                        alert("添加失败");
+                    }
+                },
+                error(resp){
+                    alert("添加失败");
+                    console.log(resp);
+                }
+        });
+    }
 }
 
 </script>
