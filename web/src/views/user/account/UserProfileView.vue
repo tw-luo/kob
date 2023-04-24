8e08a091b1237af84f76547ab52b380f99da8f72
tw_luo <2505412775@qq.com>
2023-04-24 11:52:29 +0800

    添加个人简介页面

---
+++ web/src/views/user/account/UserProfileView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <div class="card" style="margin-top: 20px">
+          <div class="card-body">
+            <img :src="$store.state.user.photo" alt="" style="width: 100%" />
+          </div>
+        </div>
+      </div>
+
+      <div class="col-9">
+        <div class="card" style="margin-top: 20px">
+          <div class="card-body">
+            
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex';
+
+export default{
+    name: 'UserProfileView',
+    setup() {
+        const store = useStore();
+        return { store };
+    },
+}
+
+</script>
